4a43e8341ebecc28a8a210cf831de002a9079c4a
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-05-25 17:38:00 +0530

    Create connection with localnet and request airdrop and confirm transaction and see on explorer

--- name-client/index.ts
+++ name-client/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const connection = new Connection("http://127.0.0.1:8899");
+
+async function main() {
+  const kp = new Keypair();
+
+  const sign = await connection.requestAirdrop(
+    kp.publicKey,
+    3 * LAMPORTS_PER_SOL
+  );
+
+  console.log({ sign });
+
+  await connection.confirmTransaction(sign);
+  const bal = await connection.getBalance(kp.publicKey);
+
+  console.log({ bal });
+}
+
+main();
