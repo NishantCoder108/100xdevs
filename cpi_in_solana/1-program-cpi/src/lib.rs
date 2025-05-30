b9f8cb3ad831a36e45c1081c08e7d38401c8926b
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-05-30 14:19:14 +0530

    Write CPI for simple data account (they have public and private key and they can store data and Lamports)

---
+++ cpi_in_solana/1-program-cpi/src/lib.rs
@@ -0,0 +1,38 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    instruction::{AccountMeta, Instruction},
+    program::invoke,
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    //Number of accounts is comming so first we will be `iter()` means it will go through one by one.
+    let mut iter = accounts.iter();
+    //data account is simple account , we can create `new Keypair()`, it will create public and private .
+    //We know , we can store Lamports and also data inside account and we can use them
+    //`next_account_info` it will provide access specific item that has been iterated
+    let data_account = next_account_info(&mut iter)?;
+    //`double_program_account` is other smart contract that already deployed we can iteract with double_program_contract ,thats called CPI (one program call to another program , they can do max 4 interanally)
+    // We can pass contract through client side for cpi call
+    let double_program_account = next_account_info(&mut iter)?;
+
+    //We can create instruction that will take program_id , accounts and data for signing the trasaction
+    let instruction = Instruction {
+        program_id: *double_program_account.key,
+        accounts: vec![AccountMeta::new(*data_account.key, true)],
+        data: instruction_data.to_vec(),
+    };
+
+    invoke(&instruction, &[data_account.clone()])?;
+
+    ProgramResult::Ok(())
+}
