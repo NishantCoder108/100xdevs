64fe3f460abce3b311390002c1aeb42ee37a1cac
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-06-30 04:58:39 +0530

    Add home routes for root directory api url

--- better-uptime-rust/api/src/main.rs
+++ better-uptime-rust/api/src/main.rs
@@ -3,16 +3,22 @@ pub mod routes;
 use poem::{
     EndpointExt, Route, Server, get, handler, listener::TcpListener, middleware::Tracing, web::Path,
 };
-use routes::{hello::hello, yourname::your_name};
+use routes::{
+    hello::hello,
+    internal::{health, home},
+    yourname::your_name,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let app = Route::new()
         .at("/hello/:name", get(hello))
-        .at("/yourname/:name", get(your_name));
+        .at("/yourname/:name", get(your_name))
+        .at("/health", get(health))
+        .at("/", get(home));
 
+    println!("Starting server on http://localhost:3000");
     Server::new(TcpListener::bind("0.0.0.0:3000"))
-        .name("hello-world")
         .run(app)
         .await
 }
