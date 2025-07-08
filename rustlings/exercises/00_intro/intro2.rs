5a748e996bab2ab9af50f54741be0e1a31fc59b6
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-07-08 12:12:49 +0530

    Variable1 complete

--- rustlings/exercises/00_intro/intro2.rs
+++ rustlings/exercises/00_intro/intro2.rs
@@ -1,4 +1,14 @@
 fn main() {
     // TODO: Fix the code to print "Hello world!".
-    printline!("Hello world!");
+    println!("Hello world!");
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    println!("{}", spaces);
+
+    let mut spaces = "     ";
+    let spaces = spaces.len(); // here if we remove let , it will give error. shadowing work with same variable name but different types
+
+    println!("{}", spaces);
 }
