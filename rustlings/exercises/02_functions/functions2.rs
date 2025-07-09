49d48b1f41eaf6e4a502f8da673ca9b2e06f8e5e
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-07-09 06:10:43 +0530

    Complete Functions

--- rustlings/exercises/02_functions/functions2.rs
+++ rustlings/exercises/02_functions/functions2.rs
@@ -1,8 +1,10 @@
 // TODO: Add the missing type of the argument `num` after the colon `:`.
-fn call_me(num:) {
+fn call_me(num: i32) -> u8 {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
+
+    3
 }
 
 fn main() {
